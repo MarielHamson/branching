7e901458d0681e161f31fc339feb15cd9bec95e2
Ali Kinsella <ali.kinsella@gmail.com>
2020-04-28 21:36:47 -0500

    create a very informative page about some animals using javascript, css, and html

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $("#orca").click(function() {
+    $(".orca-showing").toggle();
+  });
+  $("#panda").click(function() {
+    $(".panda-showing").toggle();
+  });
+  $("#penguin").click(function() {
+    $(".penguin-showing").toggle();
+  });
+});
